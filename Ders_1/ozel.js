d955828d19f61bb1076a05c499b9f6d9c85ccce6
Kamil Kalkan (kamilklkn) <zktasarim@gmail.com>
2019-02-08 14:01:51 +0300

    Değişken tanımlama, özel karakterler, değişken tipleri nasıl öğrenilir bunlara bakıldı.

---
+++ Ders_1/ozel.js
@@ -0,0 +1,50 @@
+
+//alert("Udemy javasicrip kuırsuna hoşgeldin");
+//console.log("Udemy kursuna hoşgeldiniz");
+//var e="kamil";
+//console.log(e);
+
+/*
+    1. Boşluklar dikkate alınmaz,
+    2. Türkçe karakterler kullanılmaz,
+    3. Küçük büyük harfe karşı duyarlı bir dil,
+    4. Değişken tanımlama "var" iel yapılır,
+*/
+
+var Emrah=5;
+var Emrah=10;
+
+console.log("Toplam: "+(Emrah+Emrah));
+//------------------- ÖZEL KARAKTERLER -------------------//
+
+var text1= "Kamil Kalkan \"Udemy\" kursları";
+var text2= "Kamil Kalkan \"Udemy\" \n kursları";
+var text3= "Kamil Kalkan \"Udemy\" kursları";
+document.write(text1);
+document.write("<br>-----<br>");
+document.write(text2);
+document.write("<br>-----<br>");
+//alert(text2);
+
+//------------------- DEĞİŞKEN İSİMLERİ  -------------------//
+
+/*
+    1. Değişken isimleri _ ile başlayabilir.
+    2. Değişken isimleri 235 karakter uzunluğundan fazla olmaz.
+    3. Büyük küçük harf duyarlılığı var.
+    4. Türkçe karakter kullanılmaz.
+    5. Rakamkm ile başlayamaz.
+    6. Atama işlemi = ile yapılır.
+*/
+
+//------------------- DEĞİŞKENLER  -------------------//
+
+// Değişken türünü öğrenme
+    var name="Kamil Kalkan";
+    console.log(name+" Bu bir "+ typeof name + " değişkendir.");
+
+//Var 
+    var number=12; //"" kullanmazsak number sayı olarak atama yapar.
+    console.log(number + " Bu bir " + typeof + " değişkendeğişkendir.");    
+
+
